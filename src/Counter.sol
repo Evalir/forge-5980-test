3c6ff32bbc028ece3fa3c6db39212b0031095c85
James Wenzel <jameswenzel@users.noreply.github.com>
2023-10-16 15:57:20 -0700

    add dependency

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {MyContract} from "forge-5980-test-dep/MyContract.sol";
 
+contract Counter is MyContract {
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
